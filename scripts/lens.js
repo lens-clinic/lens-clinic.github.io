b3d08e2edde70372c1dd8858f72818caae07c836
Anton Tayanovskyy (@t0yv0) <anton.tayanovskyy@gmail.com>
2013-11-17 14:28:01 -0500

    Fix maps.

---
+++ scripts/lens.js
@@ -0,0 +1,31 @@
+(function () {
+
+    function setBackground() {
+	var k = 1 + Math.floor(Math.random()*4);
+	$.backstretch("/images/bg" + String(k) + ".jpg");
+    }
+
+    function initMaps() {
+	var gm = google.maps;
+        var opts =
+	    {
+		center: new gm.LatLng(51.502298, 31.285175),
+		zoom: 18,
+		mapTypeId: gm.MapTypeId.ROADMAP
+            };
+	$(function () {
+	    $(".gmap").each(function () {
+		console.log("GMAP AT", this);
+		var dom = this;	    
+		var map = new gm.Map(dom, opts)
+	    });
+	});
+    }
+
+    function main() {
+	setBackground();
+	initMaps();
+    }
+    
+    main();
+})();
